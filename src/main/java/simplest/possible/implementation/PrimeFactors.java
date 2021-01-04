b12cf418100614df99678eaf4eba4ec9721a28ac
pooja-kandoi <pooja.kandoi@thoughtworks.com>
2021-01-04 14:23:43 +0530

    Added Implementation

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,15 @@
+package simplest.possible.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+    List<Integer> Factors;
+    public PrimeFactors(){
+        Factors=new ArrayList<Integer>();
+    }
+
+    public <T> List<Integer> of(int i) {
+        return Factors;
+    }
+}
